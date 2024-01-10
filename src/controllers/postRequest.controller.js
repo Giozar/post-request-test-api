962a306c6511c7ab32f80e4c80bb93f1e92999cb
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-01-10 09:59:16 -0600

    Complemento de Headers para cors implementado

--- src/controllers/postRequest.controller.js
+++ src/controllers/postRequest.controller.js
@@ -10,7 +10,7 @@ export async function postRequest(req, res) {
     }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     try {
         res.send({
